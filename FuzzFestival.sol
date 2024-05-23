5bfa89ed93ef6649945eb405b2479bde181b6cba
Huỳnh Trần Khanh <qcdz9r6wpcbh59+github@gmail.com>
2024-05-23 20:46:31 +0700

    Add sendNormal

--- FuzzFestival.sol
+++ FuzzFestival.sol
@@ -4,11 +4,18 @@ import "./ForcedEtherTransfer.sol";
 contract FuzzFestival {
     ForcedEtherTransfer public forcedEtherTransfer;
     FestivalContract public festivalContract;
+    uint256 counter;
     constructor() {
         forcedEtherTransfer = new ForcedEtherTransfer();
         festivalContract = new FestivalContract();
+        counter = 0;
     }
     function shootEther(address payable _target) external payable {
         forcedEtherTransfer.shoot{value: msg.value}(_target);
+    }
+    function sendNormal() external payable {
+        require(msg.value >= 1, "");
+        festivalContract.contribute{value: 1 ether}();
+        counter = counter + 1;
     }
 }
