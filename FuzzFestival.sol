b699527ad7e92bcad69151327342fc8000222761
Huỳnh Trần Khanh <qcdz9r6wpcbh59+github@gmail.com>
2024-05-23 20:32:06 +0700

    Add shootEther function

--- FuzzFestival.sol
+++ FuzzFestival.sol
@@ -1,2 +1,14 @@
 import "./Festival.sol";
 import "./ForcedEtherTransfer.sol";
+
+contract FuzzFestival {
+    ForcedEtherTransfer public forcedEtherTransfer;
+    FestivalContract public festivalContract;
+    constructor() {
+        forcedEtherTransfer = new ForcedEtherTransfer();
+        festivalContract = new FestivalContract();
+    }
+    function shootEther(address payable _target) external payable {
+        forcedEtherTransfer.shoot{value: msg.value}(_target);
+    }
+}
