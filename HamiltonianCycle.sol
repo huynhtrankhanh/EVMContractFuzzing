ce73b720a97b8558f2214b5b5becd9bb0873223f
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2024-05-23 08:31:06 +0000

    Test forced Ether reception

--- HamiltonianCycle.sol
+++ HamiltonianCycle.sol
@@ -1,5 +1,26 @@
 pragma solidity ^0.8.0;
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ForcedEtherTransfer {
+    
+    // Function to forcefully send Ether to an address
+    function shoot(address payable _target) external payable {
+        require(msg.value > 0, "Must send some ether");
+        
+        // Deploy a self-destructible contract and send ether to the target address
+        new SelfDestructContract{value: msg.value}(_target);
+    }
+}
+
+contract SelfDestructContract {
+    constructor(address payable _target) payable {
+        // Transfer all the ether stored in this contract to the target address and self-destruct
+        selfdestruct(_target);
+    }
+}
+
 contract HamiltonianCycle {
     address public owner;
     uint256 public balance;
